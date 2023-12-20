5fe3759fd223d286181035ccf2a59e5095a59e2c
xdesign-alexandrubaltazar <alexandru.baltazar@xdesign.com>
2023-12-20 14:06:27 +0000

    Make spotless work as expected

--- menu-service/menu-server/src/main/java/com/culinarycode/menuservice/MenuServiceApplication.java
+++ menu-service/menu-server/src/main/java/com/culinarycode/menuservice/MenuServiceApplication.java
@@ -3,12 +3,11 @@ package com.culinarycode.menuservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-        scanBasePackages = {
-            "com.culinarycode.menuservice",
-        })
+@SpringBootApplication(scanBasePackages = { "com.culinarycode.menuservice", })
 public class MenuServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MenuServiceApplication.class, args);
-    }
+
+	public static void main( String[] args ) {
+		SpringApplication.run( MenuServiceApplication.class, args );
+	}
+
 }
